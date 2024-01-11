3aea7b566b0fd4f2c849aeb8550cc7f178f37ceb
Xavier <odhiamboxavier@gmail.com>
2024-01-11 21:55:53 +0300

    cron time reset, cities priority

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name   : "serverapi",
     script : "./index.js",
-    cron_restart: '00 06,10,12,16 * * *',
+    cron_restart: '00 09,13,15,18 * * *',
     env_production: {
       NODE_ENV: "production"
    },
